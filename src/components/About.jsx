938a19a9fec1d9cf7d6a03e68b718845887e1740
Shakti <you@example.com>
2024-09-15 13:23:20 +0530

    hello

---
+++ src/components/About.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hello, I'm Shakti, a passionate Web developer with a keen eye for MERN
+          Stack . With a background in IT, I strive to create impactful and
+          visually stunning Software solutions that leave a lasting impression.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+          [B.Tech/Graduate],[SRMCEM,Lucknow],[2024] 
+          [Bachelor Course],[Offline/AKTU].
+          
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          Proficient in HTML,CSS,JavaScript,ReactJS,NodeJS,NextJS,mongoDB Experienced with Git/Github
+          Strong grasp of Excellent problem-solving skills Effective communicator and
+          collaborator.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Professional Experience
+        </h1>
+        <span>
+          [Web Developer],[BrandwepBTG],[12 March 2024 - 12 September] ,I completed a 6-month web developer internship,
+           gaining experience with frontend technologies like React.js, Next.js,
+           and backend technologies like Node.js, Express.js, along with MongoDB for the database.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Achievements & Awards
+        </h1>
+        <span>
+          [PYTHON], [IIT KANPUR], [2022].
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative Software solutions that exceed client expectations and
+          contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
